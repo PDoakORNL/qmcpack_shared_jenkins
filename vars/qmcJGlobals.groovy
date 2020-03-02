c09284003495bae21250de5118966140b53386c6
Peter Doak <doakpw@ornl.gov>
2020-03-02 10:34:30 -0500

    the docs are so bad

--- vars/qmcJGlobals.groovy
+++ vars/qmcJGlobals.groovy
@@ -1,6 +1,3 @@
 #!groovy
 
-class qmcJGlobals
-{
-  final GString maintainer_emails = "lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov"
-}
+def GString maintainer_emails = "lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov"
