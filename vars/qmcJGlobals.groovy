0a7b199195e206d3790ef529a456429f26006e50
Peter Doak <doakpw@ornl.gov>
2020-03-02 14:41:45 -0500

    Only this results in a proper GString at the pipeline level

--- vars/qmcJGlobals.groovy
+++ vars/qmcJGlobals.groovy
@@ -1,6 +1,12 @@
 #!groovy
 
+/** Global variables for the QMCPACK CI setup.  This avoids having to
+  * set them up in the web interface and track them trough web forms
+  *
+  *  Take care that even strings defined with GString do not work
+  *  for string globals. 
+  */
 class gmcJGlobals
 {
-  GString maintainer_emails = "lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov"
+  static def maintainer_emails = "lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov"
 }
