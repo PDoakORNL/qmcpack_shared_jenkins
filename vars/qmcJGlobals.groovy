9edc8272e2486e93d03e6d32d752692fa01eb1fb
Peter Doak <doakpw@ornl.gov>
2020-03-02 09:59:16 -0500

    more confusing jenkins pipeline groovy syntax

--- vars/qmcJGlobals.groovy
+++ vars/qmcJGlobals.groovy
@@ -1,1 +1,5 @@
-def maintainer_emails = 'lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov'
+
+class qmcJGlobals
+{
+  final String maintainer_emails = 'lopezmg@ornl.gov, kentpr@ornl.gov, markdewing@gmail.com, yeluo@anl.gov, moralessilva2@llnl.gov, doakpw@ornl.gov'
+}
